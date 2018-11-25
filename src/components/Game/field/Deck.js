6098ec9c6229719905c10347bc74aef4ab19f2bc
Luis Ortiz <ortizlu@icloud.com>
2018-11-25 18:30:56 -0500

    made drawing card animations render based on whos turn is it

--- src/components/Game/field/Deck.js
+++ src/components/Game/field/Deck.js
@@ -7,11 +7,18 @@ const Deck = (props) => {
 
   }
 
+  let animation
+  if (!props.game.turn) {
+    animation = 'field__card-behind-deck field__card-behind-deck--animated-one'
+  } else {
+    animation = 'field__card-behind-deck field__card-behind-deck--animated-two'
+  }
+
   return (
     <div>
       <img onClick={props.deck.drawingAllowed ? props.deckDraw : def} src="./img/deck.png" className="field__deck">
       </img>
-      <img src="./img/hidden.png" className={props.deck.drawingAnimations ? 'field__card-behind-deck field__card-behind-deck--animated-one' : 'field__card-behind-deck'}></img>
+      <img src="./img/hidden.png" className={props.deck.drawingAnimations ? animation : 'field__card-behind-deck'}></img>
     </div>
 
   );
